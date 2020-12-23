f79adbd30fb7d77de591196001884c578a4e384b
prerana1821 <prerananawar1@gmail.com>
2020-12-24 03:25:04 +0530

    update: Added Loading Spinner

--- palindrome-birthday/app.js
+++ palindrome-birthday/app.js
@@ -1,96 +1,123 @@
 const birthDate = document.querySelector('#birth-date');
 const btn = document.querySelector('#btn');
 const output = document.querySelector('#output');
+const loader = document.querySelector('#loading');
+const reset = document.querySelector('#reset');
 
+let dateFormats;
 btn.addEventListener('click', function() {
-    let date = birthDate.value;
-    // console.log(date);
-    if (date === '') {
-        alert('Please, Enter your birthdate!')
-    } else {
-        let formats = dateFormat(date);
-        // console.log(formats);
-        // let formats = ['123', '190', '021', '876'];
-        // let formats = ['123', '223', '121', '876'];
-        // console.log(output.value);
-        // formats.forEach(date => {
-        //     // console.log(date);
-        //     //  palindromeNum(date);
-        //     if (palindromeNum(date)) {
-        //         console.log('True');
-        //         output.innerText = 'Palindrome';
-        //         return;
-        //     }
-        //     // else {
-        //     //     console.log('false');
-        //     // }
-        // });
-
-        for (let i = 0; i < formats.length; i++) {
-            if (palindromeNum(formats[i])) {
-                console.log('True');
-                output.innerText = 'Palindrome';
-            }
+    // console.log(birthDate.value);
+    dateFormats = dateFormat(birthDate.value);
+    // let dateFormats = ['119', '201', '1981', '876']
+    // console.log(dateFormats[0]);
+    let result = 0;
+    for (let i = 0; i < dateFormats.length; i++) {
+        if (palindromeNum(dateFormats[i])) {
+            // console.log('true');
+            result = result + 1;
+            loader.className = "show";
+            output.className = "hide";
+            setTimeout(function() {
+                loader.className = loader.className.replace("show", "");
+                output.className = output.className.replace("hide", "")
+            }, 1000);
+            console.log('Palindrome');
+            output.innerText = 'Congralulations! \n Your Birthdate is a Palindrome Date🎉🔥';
         }
-
-        if (output.innerText === undefined || output.innerText.indexOf('a') === -1) {
-            // console.log('Hello');
-            let nearest = nearestPalindromeNum(formats[0]);
-            console.log(formats[0]);
-            console.log(nearest);
-            let nearestString = nearest.toString();
-            // let nearestYear = nearestString.slice(0, 4);
-            // let nearestMonth = nearestString.slice(4, 6);
-            // let nearestDate = nearestString.slice(6, 8);
-
-            let dateTemp1 = `${formats[0].slice(0, 4)}/${formats[0].slice(4, 6)}/${formats[0].slice(6, 8)}`;
-            console.log(dateTemp1);
-
-            let dateTemp2 = `${nearestString.slice(0, 4)}/${nearestString.slice(4, 6)}/${nearestString.slice(6, 8)}`;
-            console.log(dateTemp2);
-
-
-            let date1 = new Date(dateTemp1);
-            let date2 = new Date(dateTemp2);
-            let diffTime = Math.abs(date2 - date1);
-            let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-            console.log(diffDays + " days");
-
-            output.innerText = `${dateTemp2} ${diffDays} Days`;
-
-        }
-
     }
-
-
+    if (result === 0) {
+        // console.log('Hello');
+        let nearestDate = nearestPalindromeNum(dateFormats[0]);
+        console.log(nearestDate);
+    }
 });
 
-// palindromeNum(1001);
 
 function nearestPalindromeNum(num) {
-    let low = num - 1;
-    // console.log(low);
-    while (!palindromeNum(low)) {
-        low = low - 1;
-    }
-    // console.log(low);
+    const strNum = String(num);
+    const half = strNum.substring(0, Math.floor(strNum.length / 2));
+    const reversed = half.split("").reverse().join("");
+    const first = strNum.length % 2 === 0 ? half : strNum.substring(0,
+        Math.ceil(strNum.length / 2))
 
-    let high = parseInt(num) + 1;
-    // console.log(high);
-    while (!palindromeNum(high)) {
-        high = high + 1;
-    }
-    // console.log(high);
-
-    if (low < high) {
-        console.log(low);
-        return low;
+    let sum = first + reversed;
+    // console.log(sum);
+    // console.log(sum.slice(4, 6));
+    // console.log(sum.slice(6, 8));
+    if (sum.slice(4, 6) <= 12 && sum.slice(6, 8) <= 31) {
+        return sum;
     } else {
-        console.log(high);
-        return high;
+        nearestPalindromeNum(sum);
     }
-
+    // return +(first + reversed);
+    // validate(first + reversed);
 }
+
+function validate(date) {
+    let dateString = date.toString();
+    let dateTemp1 = `${dateString.slice(0, 4)}/${dateString.slice(4, 6)}/${dateString.slice(6, 8)}`;
+    // console.log(dateTemp1);
+    let isValidDate = Date.parse(dateTemp1);
+    if (isNaN(isValidDate)) {
+        // when is not valid date logic
+        // return false;
+        // console.log('Hello');
+        nearestPalindromeNum(date);
+        // differnce date 
+    } else {
+        console.log('Hii');
+        console.log(date);
+    }
+}
+
+// console.log(findNearestPalindrome('20170723'));
+
+//YYYY-MM-DD
+// function nearestPalindromeNum(num) {
+//     // console.log(num);
+//     let low = num - 1;
+//     while (!palindromeNum(low)) {
+//         low = low - 1;
+//     }
+//     // console.log(low);
+
+//     let high = parseInt(num) + 1;
+//     while (!palindromeNum(high)) {
+//         high = high + 1;
+//     }
+//     // console.log(high);
+
+//     // dhigh = new Date(high);
+//     // console.log(dhigh.isValid());
+//     // console.log(high.getDate());
+
+//     if (low < high) {
+//         // console.log(low);
+//         validate(low);
+//         // return low;
+//     } else {
+//         // console.log(high);
+//         // return high;
+//         validate(high);
+
+//     }
+// }
+
+// let dateTemp1 = `${low[0].slice(0, 4)}/${low[0].slice(4, 6)}/${low[0].slice(6, 8)}`;
+// console.log(dateTemp1);
+
+// let isValidDate = Date.parse('1980/02/29');
+
+// let isValidDate = Date.parse('2016/61/02');
+// if (isNaN(isValidDate)) {
+//     // when is not valid date logic
+//     // return false;
+//     console.log('Hello');
+// } else {
+//     console.log('Hii');
+// }
+
+// console.log(palindromeNum(123021));
 
 function palindromeNum(number) {
     let reverse = 0;
@@ -100,7 +127,6 @@ function palindromeNum(number) {
         reverse = reverse * 10 + lastDigit;
         number = parseInt(number / 10);
     }
-
     if (originalNumber == reverse) {
         // console.log('Palindrome');
         return true;
@@ -111,17 +137,13 @@ function palindromeNum(number) {
 }
 
 function dateFormat(date) {
-
     let formats = [];
-
     // YYYY - MM - DD
     // MM - DD - YYYY
     // DD - MM - YYYY
-
     // YY - MM - DD
     // MM - DD - YY
     // DD - MM - YY
-
     let dateArray = date.split('-');
     let year = dateArray[0];
     let month = dateArray[1];
@@ -156,3 +178,8 @@ function dateFormat(date) {
     // console.log(formats);
     return formats;
 }
+
+reset.addEventListener('click', function() {
+    birthDate.value = '';
+    output.innerText = '';
+});
